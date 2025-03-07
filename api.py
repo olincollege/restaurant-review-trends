d3d109f83f6fafc964d6f5812572556f80857921
Mira Epstein <mepstein@olin.edu>
2025-03-07 11:42:50 -0500

    Commit api.py.

--- api.py
+++ api.py
@@ -1,10 +1,18 @@
 import requests
 import apikeys
 
-url = f"https://api.content.tripadvisor.com/api/v1/location/locationId/reviews?category=restaurant&key={apikeys.key_name}language=en&limit=2"
+"""
+url: A string containing the url with data points we want to pull.
+headers: 
+"""
+
+# url = f"https://api.content.tripadvisor.com/api/v1/location/60745/reviews?category=restaurant&key={apikeys.key_name}&language=en&limit=2"
+
+url = f"https://api.content.tripadvisor.com/api/v1/location/60745/reviews?category=restaurant&language=en&limit=2&rating=1&key={apikeys.key_name}"
+
 
 headers = {"accept": "application/json"}
 
-response = requests.api.get(url, headers=headers, timeout=10)
+response = requests.api.get(url, headers=headers, timeout=2)
 
 print(response.text)
